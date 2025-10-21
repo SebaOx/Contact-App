20fb2d1bbc9f39baf76d067f592ff85a7b1948ea
Sebastián <soxandabarat13@gmail.com>
2025-10-21 18:34:11 -0300

    Delete de contacto

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php
+
+require "database.php";
+
+$id = $_GET["id"];
+
+//Para evitar que intenten borrar un contacto con id inexistente desde consola o modificando la url
+$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+$statement->execute([":id" => $id]);
+
+if($statement->rowCount() == 0){
+    //si no existe el id redirigimos a la pagina principal
+    http_response_code(404);
+    echo("HTTP 404 NOT FOUND");
+    return;
+}
+
+//ahora si logica general para borrado dedse la pag web
+$conn->prepare("DELETE FROM contacts WHERE id = :id")->execute([":id" => $id]);
+
+
+header("Location: index.php");
