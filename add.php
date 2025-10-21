abf5a7bc4b1b59c030ddb3fa681200ca37108303
Sebastián <soxandabarat13@gmail.com>
2025-10-21 08:30:10 -0300

    Validacion y bind parameters

--- add.php
+++ add.php
@@ -1,15 +1,28 @@
 <?php
 
 require "database.php";
+//Definimos error y lo usaremos más adelante
+$error = null;
 // Variable superglobales "$_algo", disponible en cualquier archivo de php 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $phoneNumber = $_POST["phone_number"];
+    // Validaciones de los campos
+    if (empty($_POST["name"]) || empty($_POST["phone_number"])) {
+      $error = "Por favor rellene todos los campos.";
+    }else if (strlen($_POST["phone_number"]) < 9) {
+      $error = "EL numero de telefono debe ser de al menos 9 dígitos.";
+    }else{
+      $name = $_POST["name"];
+      $phoneNumber = $_POST["phone_number"];
 
-    $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES ('$name', '$phoneNumber')");
-    $statement->execute();
+      //se maneja con sentencias cuando se agrega un contacto para evitar inyecciones SQL
+      $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES (:name, :phone_number)");
+      $statement->bindParam(":name", $_POST["name"]);
+      $statement->bindParam(":phone_number", $_POST["phone_number"]);
+      $statement->execute();
 
-    header("Location: index.php");
+      header("Location: index.php");
+    }
+    
   }
 
 ?>
@@ -78,6 +91,11 @@ require "database.php";
           <div class="card">
             <div class="card-header">Add New Contact</div>
             <div class="card-body">
+              <?php if ($error): ?>
+                <p class="text-danger">
+                  <?= $error ?>
+                </p>
+              <?php endif ?>
               <form method="POST" action="add.php">
                 <div class="mb-3 row">
                   <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
