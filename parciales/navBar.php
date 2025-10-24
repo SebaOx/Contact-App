b0a5a1f3ff783393d4dc63e1d4afe0c17260ae31
Sebastián <soxandabarat13@gmail.com>
2025-10-24 09:20:33 -0300

    registro de usuarios en BD

--- parciales/navBar.php
+++ parciales/navBar.php
@@ -17,11 +17,18 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a class="nav-link" href="home.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="add.php">Add Contact</a>
+        </li> -->
+        <!-- botones para registrarte o logearte -->
+         <li class="nav-item">
+          <a class="nav-link" href="login.php">Login</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="register.php">Register</a>
         </li>
       </ul>
     </div>
---
+++ register.php
@@ -0,0 +1,94 @@
+<?php
+
+require "database.php";
+//Definimos error y lo usaremos más adelante
+$error = null;
+// Variable superglobales "$_algo", disponible en cualquier archivo de php 
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // chequeamos que no esten vacios los campos
+    if (empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["password"])) {
+      $error = "Please fill all the fields.";
+      // y que el correo tenga el formato adecuado
+    } else if (!str_contains($_POST["email"], "@")){
+        $error = "Email format is incorrect.";
+        // ahora si tomamos los datos
+    } else {
+        // tomamos el correo que nos pasaa el usuario para verificar que no exista
+        // ningun usuario en la base con ese email
+        $statement = $conn->prepare("SELECT * FROM users WHERE email = :email");
+        $statement->bindParam(":email", $_POST["email"]);
+        $statement->execute();
+        //si existe mandamos el error
+        if ($statement->rowCount() > 0) {
+          $error = "This email is taken.";
+          
+        } else {
+          //sino creamos el usuario en la tabla tomando los datos que nos pasan
+          $conn
+            ->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)")
+            ->execute([
+                ":name" => $_POST["name"],
+                ":email" => $_POST["email"],
+                ":password" => password_hash($_POST["password"], PASSWORD_BCRYPT)
+            ]);
+
+            header("Location: home.php");
+        }
+    }
+  }
+
+?>
+
+<!-- import del header -->
+<?php require "parciales/header.php" ?>
+
+<div class="container pt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Register</div>
+        <div class="card-body">
+          <?php if ($error): ?>
+            <p class="text-danger">
+              <?= $error ?>
+            </p>
+          <?php endif ?>
+          <form method="POST" action="register.php">
+            <div class="mb-3 row">
+              <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
+
+              <div class="col-md-6">
+                <input id="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
+              </div>
+            </div>
+
+            <div class="mb-3 row">
+              <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
+
+              <div class="col-md-6">
+                <input id="email" type="tel" class="form-control" name="email" required autocomplete="email" autofocus>
+              </div>
+            </div>
+
+            <div class="mb-3 row">
+              <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
+
+              <div class="col-md-6">
+                <input id="password" type="tel" class="form-control" name="password" required autocomplete="password" autofocus>
+              </div>
+            </div>
+
+            <div class="mb-3 row">
+              <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- import del footer -->
+<?php require "parciales/footer.php" ?>
